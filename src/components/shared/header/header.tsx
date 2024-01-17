7a2211957b8fdd35b2e39db02574ed7f88af3b51
ndelanhese <nnathanh3@gmail.com>
2024-01-17 12:22:45 -0300

    feat(theme): add theme toggle

--- src/components/shared/header/header.tsx
+++ src/components/shared/header/header.tsx
@@ -1,25 +1,29 @@
-import { NavLink } from '@components/shared/navLink';
-import { Separator } from '@components/ui/separator';
-import { Home, Pizza, UtensilsCrossed } from 'lucide-react';
+import { NavLink } from "@components/shared/navLink";
+import { ThemeToggle } from "@components/theme/themeToggle";
+import { Separator } from "@components/ui/separator";
+import { Home, Pizza, UtensilsCrossed } from "lucide-react";
 
 export const Header = () => {
-  return (
-    <header className="border-b">
-      <div className="flex h-16 items-center gap-6 px-6">
-        <Pizza className="h-6 w-6" />
-        <Separator orientation="vertical" className="h-6" />
+	return (
+		<header className="border-b">
+			<div className="flex h-16 items-center gap-6 px-6">
+				<Pizza className="h-6 w-6" />
+				<Separator orientation="vertical" className="h-6" />
 
-        <nav className="flex items-center space-x-4 lg:space-x-6">
-          <NavLink href={'/dashboard'}>
-            <Home className="h-4 w-4" />
-            Início
-          </NavLink>
-          <NavLink href={'/orders'}>
-            <UtensilsCrossed className="h-4 w-4" />
-            Pedidos
-          </NavLink>
-        </nav>
-      </div>
-    </header>
-  );
+				<nav className="flex items-center space-x-4 lg:space-x-6">
+					<NavLink href={"/dashboard"}>
+						<Home className="h-4 w-4" />
+						Início
+					</NavLink>
+					<NavLink href={"/orders"}>
+						<UtensilsCrossed className="h-4 w-4" />
+						Pedidos
+					</NavLink>
+				</nav>
+				<div className="ml-auto flex items-center gap-2">
+					<ThemeToggle />
+				</div>
+			</div>
+		</header>
+	);
 };
