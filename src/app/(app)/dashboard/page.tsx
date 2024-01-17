356afaba96e16e63ba8d3d681a82facdc107c14c
ndelanhese <nnathanh3@gmail.com>
2024-01-17 17:50:37 -0300

    fix(dashboard): fix dashboard title

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
 
 const DashboardPage = () => (
 	<section className="flex flex-col gap-4">
-		<h1 className="text-3xl font-bold tracking-tight">Pedidos</h1>
+		<h1 className="text-3xl font-bold tracking-tight">Dashboard</h1>
 		<div className="grid grid-cols-4 gap-4 mt-4">
 			<MonthRevenueCard />
 			<MonthOrdersAmountCard />
