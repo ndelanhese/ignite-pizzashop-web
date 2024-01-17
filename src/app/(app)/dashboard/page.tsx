b65425549b3cf79c3b2e12fd14f8a61de2428167
ndelanhese <nnathanh3@gmail.com>
2024-01-17 17:41:43 -0300

    feat(dashboard): charts

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -1,9 +1,30 @@
-import { Metadata } from 'next';
+import { DayOrdersAmountCard } from "@components/shared/dayOrdersAmountCard";
+import { MonthCanceledOrdersAmountCard } from "@components/shared/monthCanceledOrdersAmountCard";
+import { MonthOrdersAmountCard } from "@components/shared/monthOrdersAmountCard";
+import { MonthRevenueCard } from "@components/shared/monthRevenueCard";
+import { PopularProductsChart } from "@components/shared/popularProductsChart";
+import { RevenueChart } from "@components/shared/revenueChart";
+import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: 'Dashboard',
+	title: "Dashboard",
 };
 
-const DashboardPage = () => <h1>Dashboard</h1>;
+const DashboardPage = () => (
+	<section className="flex flex-col gap-4">
+		<h1 className="text-3xl font-bold tracking-tight">Pedidos</h1>
+		<div className="grid grid-cols-4 gap-4 mt-4">
+			<MonthRevenueCard />
+			<MonthOrdersAmountCard />
+			<DayOrdersAmountCard />
+			<MonthCanceledOrdersAmountCard />
+		</div>
+
+		<div className="grid grid-cols-9 gap-4">
+			<RevenueChart />
+			<PopularProductsChart />
+		</div>
+	</section>
+);
 
 export default DashboardPage;
--- src/app/(app)/orders/page.tsx
+++ src/app/(app)/orders/page.tsx
@@ -6,11 +6,9 @@ export const metadata: Metadata = {
 };
 
 const OrdersPage = () => (
-	<section>
-		<div className="flex flex-col gap-4">
-			<h1 className="text-3xl font-bold tracking-tight">Pedidos</h1>
-		</div>
-		<div className="space-y-2.5 mt-4">
+	<section className="flex flex-col gap-4">
+		<h1 className="text-3xl font-bold tracking-tight">Pedidos</h1>
+		<div className="space-y-2.5">
 			<OrderTable />
 		</div>
 	</section>
