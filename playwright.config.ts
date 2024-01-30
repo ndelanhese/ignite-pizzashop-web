9212a84b35d3a59ebb1c5b6863b13cfe13902549
ndelanhese <nnathanh3@gmail.com>
2024-01-30 11:17:39 -0300

    tests(test): add tests config

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,103 +1,105 @@
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig, devices } from "@playwright/test";
 
 // Use process.env.PORT by default and fallback to port 3000
 const PORT = process.env.NEXT_PUBLIC_APP_PORT || 3000;
-const BASE_URL = process.env.NEXT_PUBLIC_APP_URL || 'http://127.0.0.1';
+const BASE_URL = process.env.NEXT_PUBLIC_APP_URL || "http://127.0.0.1";
 
 // Set webServer.url and use.baseURL with the location of the WebServer respecting the correct set port
 const baseURL = new URL(`${BASE_URL}:${PORT}`);
 
 export default defineConfig({
-  // Timeout per test
-  timeout: 30 * 1000,
+	// Timeout per test
+	timeout: 30 * 1000,
 
-  // Artifacts folder where screenshots, videos, and traces are stored.
-  outputDir: 'test-results/',
+	// Artifacts folder where screenshots, videos, and traces are stored.
+	outputDir: "test-results/",
 
-  // Run all tests in parallel.
-  fullyParallel: true,
+	testIgnore: "**/src/components/**",
 
-  // Fail the build on CI if you accidentally left test.only in the source code.
-  forbidOnly: !!process.env.CI,
+	// Run all tests in parallel.
+	fullyParallel: true,
 
-  // Retry on CI only.
-  retries: process.env.CI ? 2 : 0,
+	// Fail the build on CI if you accidentally left test.only in the source code.
+	forbidOnly: !!process.env.CI,
 
-  // Opt out of parallel tests on CI.
-  workers: process.env.CI ? 1 : undefined,
+	// Retry on CI only.
+	retries: process.env.CI ? 2 : 0,
 
-  // Reporter to use
-  reporter: 'html',
+	// Opt out of parallel tests on CI.
+	workers: process.env.CI ? 1 : undefined,
 
-  // Run your local dev server before starting the tests:
-  // https://playwright.dev/docs/test-advanced#launching-a-development-web-server-during-the-tests
-  webServer: {
-    command: 'bun dev',
-    url: baseURL.toString(),
-    timeout: 120 * 1000,
-    reuseExistingServer: !process.env.CI,
-  },
+	// Reporter to use
+	reporter: "html",
 
-  use: {
-    // Use baseURL so to make navigations relative.
-    // More information: https://playwright.dev/docs/api/class-testoptions#test-options-base-url
-    baseURL: baseURL.toString(),
+	// Run your local dev server before starting the tests:
+	// https://playwright.dev/docs/test-advanced#launching-a-development-web-server-during-the-tests
+	webServer: {
+		command: "bun dev",
+		url: baseURL.toString(),
+		timeout: 120 * 1000,
+		reuseExistingServer: !process.env.CI,
+	},
 
-    // Retry a test if its failing with enabled tracing. This allows you to analyze the DOM, console logs, network traffic etc.
-    // More information: https://playwright.dev/docs/trace-viewer
+	use: {
+		// Use baseURL so to make navigations relative.
+		// More information: https://playwright.dev/docs/api/class-testoptions#test-options-base-url
+		baseURL: baseURL.toString(),
 
-    // Collect trace when retrying the failed test.
-    trace: 'on-first-retry',
+		// Retry a test if its failing with enabled tracing. This allows you to analyze the DOM, console logs, network traffic etc.
+		// More information: https://playwright.dev/docs/trace-viewer
 
-    // Capture screenshot after each test failure.
-    screenshot: 'only-on-failure',
+		// Collect trace when retrying the failed test.
+		trace: "on-first-retry",
 
-    // All available context options: https://playwright.dev/docs/api/class-browser#browser-new-context
-    // contextOptions: {
-    //   ignoreHTTPSErrors: true,
-    // },
-  },
+		// Capture screenshot after each test failure.
+		screenshot: "only-on-failure",
 
-  projects: [
-    {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
-    },
+		// All available context options: https://playwright.dev/docs/api/class-browser#browser-new-context
+		// contextOptions: {
+		//   ignoreHTTPSErrors: true,
+		// },
+	},
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+	projects: [
+		{
+			name: "chromium",
+			use: { ...devices["Desktop Chrome"] },
+		},
 
-    // {
-    //   name: 'webkit',
-    //   use: { ...devices['Desktop Safari'] },
-    // },
+		{
+			name: "firefox",
+			use: { ...devices["Desktop Firefox"] },
+		},
 
-    /* Test against mobile viewports. */
-    {
-      name: 'Mobile Chrome',
-      use: { ...devices['Pixel 5'] },
-    },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
+		// {
+		//   name: 'webkit',
+		//   use: { ...devices['Desktop Safari'] },
+		// },
 
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: {
-    //     ...devices['Desktop Edge'],
-    //     channel: 'msedge',
-    //   },
-    // },
-    {
-      name: 'Google Chrome',
-      use: {
-        ...devices['Desktop Chrome'],
-        channel: 'chrome',
-      },
-    },
-  ],
+		/* Test against mobile viewports. */
+		{
+			name: "Mobile Chrome",
+			use: { ...devices["Pixel 5"] },
+		},
+		// {
+		//   name: 'Mobile Safari',
+		//   use: { ...devices['iPhone 12'] },
+		// },
+
+		/* Test against branded browsers. */
+		// {
+		//   name: 'Microsoft Edge',
+		//   use: {
+		//     ...devices['Desktop Edge'],
+		//     channel: 'msedge',
+		//   },
+		// },
+		{
+			name: "Google Chrome",
+			use: {
+				...devices["Desktop Chrome"],
+				channel: "chrome",
+			},
+		},
+	],
 });
